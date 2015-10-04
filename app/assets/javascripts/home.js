603a3677116480512d31669506f1d91e492d507a
Rajat Subhra Ghosh <rajatsubhra.ghosh@gmail.com>
2015-10-04 16:34:33 -0700

    removed unused files

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,6 @@
 //= require_self
 //= require_tree ./modules
 //= require_tree ./services/global
-//= require_tree ./services/home
-//= require_tree ./filters/global
-//= require_tree ./filters/home
 //= require_tree ./controllers/global
 //= require_tree ./controllers/home
-//= require_tree ./directives/global
-//= require_tree ./directives/home
 
