b0541a57dc105f1e6664153fac6da2446d7a5ca5
Rajat Subhra Ghosh <rajatsubhra.ghosh@gmail.com>
2015-10-05 11:41:16 -0700

    fixed error message

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,4 +4,13 @@ class Feedback < ActiveRecord::Base
   do_not_validate_attachment_file_type :attachment
   validates_attachment_presence :attachment, :allow_blank => true
   validates_attachment_size :attachment, { :in => 0..2.megabytes }
+
+  after_validation :clean_paperclip_errors
+
+
+  private
+
+  def clean_paperclip_errors
+  	errors.delete(:attachment)
+  end
 end
